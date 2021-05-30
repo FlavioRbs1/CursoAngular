a8613fe86fdbf87ab9b0f0ccdc3e5d3df5d52103
Flavio Ribeiro <flavio.ribeiro@daxia.local>
2021-05-30 18:30:37 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { CourseListComponent } from 'src/courses/course-list.component';
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
